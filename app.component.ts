6847cddc0265281a0976da0bd0a1031bc3517e99
francisco202178 <142852894+francisco202178@users.noreply.github.com>
2023-09-12 14:49:43 -0600

    Initial commit

---
+++ app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from 
+'@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent {
+  name = 'Angular';
+  img =
+  'https://cdn2.thecatapi.com/images/49f.gif' ;
+}
